c4333a929417f14f9502836b04bb46d3584c3e06
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-09-20 05:58:47 +0900

    feat: run test to make sure bundler works

--- cprs_cli/src/cli/build.rs
+++ cprs_cli/src/cli/build.rs
@@ -1,14 +1,32 @@
 use crate::{build::bundler, task::Task};
 
 use super::{cmd::Build, Run};
-use anyhow::Result;
-use tokio::fs;
+use anyhow::{Context, Result};
+use tokio::{fs, process};
 
 impl Run for Build {
     async fn run(&self) -> Result<()> {
         let task = Task::from_current_dir().await?;
         let code = bundler::bundle_task(&task)?;
-        fs::write(task.submit_file, code).await?;
+        fs::write(&task.submit_file, code).await?;
+
+        // rerun test to make sure everything ok
+        let submit_bin = task
+            .submit_file
+            .file_stem()
+            .and_then(|f| f.to_str())
+            .unwrap();
+        let out = process::Command::new("cargo")
+            .args(["test", "--color", "always", "--bin", submit_bin])
+            .output()
+            .await
+            .with_context(|| format!("Failed to execute test for `{}`", submit_bin))?;
+        println!("{}", String::from_utf8(out.stderr)?);
+        println!("{}", String::from_utf8(out.stdout)?);
+        println!(
+            "You can rerun this using:\n  cargo test --bin {}",
+            submit_bin
+        );
         Ok(())
     }
 }
