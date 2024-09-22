6079ee0e692ee2a8fa15fb5188fd751a5c4f797e
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-09-22 22:40:43 +0900

    feat: implement bridge finding

---
+++ algo/src/graph/bridge.rs
@@ -0,0 +1,70 @@
+use super::base::Graph;
+
+pub trait Bridge {
+    fn bridge(self) -> DfsTree;
+}
+
+impl Bridge for Graph {
+    fn bridge(self) -> DfsTree {
+        DfsTree::new(self, 0)
+    }
+}
+
+#[derive(Debug)]
+pub struct DfsTree {
+    graph: Graph,
+    pub root: u32,
+    pub parent: Vec<Option<u32>>,
+    pub weight: Vec<u32>,
+    pub height: Vec<u32>,
+    pub bridges: Vec<(u32, u32)>,
+}
+
+impl DfsTree {
+    pub(super) fn new(graph: Graph, root: u32) -> DfsTree {
+        let n = graph.size;
+        DfsTree {
+            graph,
+            root,
+            parent: vec![None; n],
+            height: vec![0; n],
+            weight: vec![0; n],
+            bridges: Vec::new(),
+        }
+        .bridges()
+    }
+
+    fn bridges(mut self) -> DfsTree {
+        let mut visited = vec![false; self.graph.size];
+        let mut backedges_count = vec![-1; self.graph.size];
+        self.dfs(self.root, &mut visited, &mut backedges_count);
+        self.bridges = backedges_count
+            .iter()
+            .enumerate()
+            .filter(|(_, &count)| count == 0)
+            .map(|(node, _)| (node as u32, self.parent[node].unwrap()))
+            .collect();
+        self
+    }
+
+    fn dfs(&mut self, node: u32, visited: &mut [bool], backedges_count: &mut [i32]) {
+        let node = node as usize;
+        self.weight[node] += 1;
+        visited[node] = true;
+
+        for index in 0..self.graph[node].len() {
+            let child = self.graph[node][index];
+            if !visited[child as usize] {
+                self.parent[child as usize] = Some(node as u32);
+                self.height[child as usize] = self.height[node] + 1;
+                self.dfs(child, visited, backedges_count);
+                self.weight[node] += self.weight[child as usize];
+                backedges_count[node] += backedges_count[child as usize];
+            } else if self.height[node] > self.height[child as usize] {
+                backedges_count[node] += 1;
+            } else {
+                backedges_count[node] -= 1;
+            }
+        }
+    }
+}
--- algo/src/graph/mod.rs
+++ algo/src/graph/mod.rs
@@ -1,3 +1,4 @@
 pub mod base;
+pub mod bridge;
 pub mod dsu;
 pub mod scc;
